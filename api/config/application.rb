f774b89ac21fea1dc1ef3cd7ba8c2f47fb6cbd4e
Jimmy Pocock <jimmypocock@yahoo.com>
2015-05-16 11:48:10 -0500

    Got the update user method working with a little success and error notification. Updated the Rack::Cors config. Put the get Users in an IFFE so i can call it later if need be.

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,7 +28,10 @@ module JsonTestApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 0
       end
     end
 
--- front/app/users/edit/users.edit.controller.js
+++ front/app/users/edit/users.edit.controller.js
@@ -4,13 +4,33 @@ angular.module('jsonTestFrontApp')
   .controller('UsersEditCtrl', ['$scope', '$stateParams', 'API',
     function ($scope, $stateParams, API) {
       $scope.user;
+      $scope.messages = {
+        success: "Yay! You did it.",
+        error: "Not cool, dude!"
+      };
+      $scope.success = null;
 
-      API.get('/users/' + $stateParams.id)
-      .then(function(resolve){
-        $scope.user = resolve["user"];
-      })
-      .catch(function(err){
-        // return cb(err);
-      });
+      $scope.getUser = (function(){
+        API.get('/users/' + $stateParams.id)
+        .then(function(resolve){
+          $scope.user = resolve["user"];
+        })
+        .catch(function(err){
+          // return cb(err);
+        });
+      })();
+
+      $scope.updateUser = function(){
+        API.put('/users/' + $stateParams.id, {
+          "user": $scope.user
+        })
+        .then(function(resolve){
+          $scope.user = resolve["user"];
+          $scope.success = true;
+        })
+        .catch(function(err){
+          $scope.success = false;
+        });
+      }
     }
   ]);
